3e39b339f1d75470dc2a84a12bbc9b5540ed7c53
mjmartinezstudium <mjmartinezstudium@gmail.com>
2025-02-12 08:44:06 +0100

    First commit

---
+++ src/es/studium/Repositorio/Repositorio.java
@@ -0,0 +1,36 @@
+package es.studium.Repositorio;
+
+import java.util.Scanner;
+
+public class Repositorio
+{
+	public static void main(String[] args)
+	{
+		Scanner teclado = new Scanner(System.in);
+		int opcion;
+		do
+		{
+			System.out.println("Menú");
+			System.out.println("1-Sumar");
+			System.out.println("0-Salir");
+			opcion = teclado.nextInt();
+			switch(opcion)
+			{
+			case 0:
+				System.out.println("Adiós");
+				break;
+			case 1:
+				sumar();
+				break;
+			default:
+				System.out.println("Opción incorrecta");
+			}
+		}while(opcion!=0);
+		teclado.close();
+	}
+
+	private static void sumar()
+	{
+		System.out.println("Has elegido la suma");
+	}
+}
